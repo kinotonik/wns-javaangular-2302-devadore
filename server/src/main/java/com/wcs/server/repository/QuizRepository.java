59996e7de7b17b35b74fbb5b85bb1f421d046fe5
kinotonik <zedmed@kinotonik.fr>
2023-05-24 17:44:18 +0200

    fix : for delete user, in CASCADE becareful !!

--- server/src/main/java/com/wcs/server/repository/QuizRepository.java
+++ server/src/main/java/com/wcs/server/repository/QuizRepository.java
@@ -1,4 +1,11 @@
 package com.wcs.server.repository;
 
-public interface QuizRepository {
+import com.wcs.server.entity.Quiz;
+import com.wcs.server.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuizRepository extends JpaRepository< Quiz, Long> {
+    List<Quiz> findByCreatedBy(User user);
 }
--- server/src/main/java/com/wcs/server/security/GlobalExceptionHandler.java
+++ server/src/main/java/com/wcs/server/security/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     /***
