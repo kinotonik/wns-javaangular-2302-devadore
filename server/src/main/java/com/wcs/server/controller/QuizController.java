580c492795036cf9be4226a207229b3e87f2b208
AxelColliaux <axel.colliaux@hotmail.fr>
2023-07-03 11:20:12 +0200

    get Quiz ok

--- server/src/main/java/com/wcs/server/controller/QuizController.java
+++ server/src/main/java/com/wcs/server/controller/QuizController.java
@@ -1,4 +1,26 @@
 package com.wcs.server.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wcs.server.dto.QuizDTO;
+import com.wcs.server.service.QuizService;
+
+@RestController
+@RequestMapping("/api")
 public class QuizController {
+
+    @Autowired
+    private QuizService quizService;
+
+    @GetMapping("/quizs")
+    public ResponseEntity<List<QuizDTO>> getAll() {
+        List<QuizDTO> quizs = quizService.getAll();
+        return ResponseEntity.ok(quizs);
+    }
 }
--- server/src/main/java/com/wcs/server/service/QuizService.java
+++ server/src/main/java/com/wcs/server/service/QuizService.java
@@ -1,4 +1,33 @@
 package com.wcs.server.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wcs.server.dto.QuizDTO;
+import com.wcs.server.entity.Quiz;
+import com.wcs.server.repository.QuizRepository;
+
+@Service
 public class QuizService {
+
+    @Autowired
+    private QuizRepository quizRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<QuizDTO> getAll() {
+        List<Quiz> quizzes = quizRepository.findAll();
+        return quizzes.stream()
+                .map(this::convertQuizToDTO)
+                .collect(Collectors.toList());
+    }
+
+    private QuizDTO convertQuizToDTO(Quiz quiz) {
+        return modelMapper.map(quiz, QuizDTO.class);
+    }
 }
