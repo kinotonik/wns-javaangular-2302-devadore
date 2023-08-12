5b7c4b3b86e4e3a728c347e0c02092641db03b73
kinotonik <zedmed@kinotonik.fr>
2023-08-13 00:05:01 +0200

    feat: modifier les alertes par des toast tailwind + verif role de homepage

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {AuthInterceptor} from "./auth/auth.interceptor";
-import { HomeComponent } from './pages/home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HomeComponent} from './pages/home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatTableModule} from "@angular/material/table";
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { AnimatedSlideComponent } from './pages/home/components/animated-slide/animated-slide.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {AnimatedSlideComponent} from './pages/home/components/animated-slide/animated-slide.component';
+import {ToastService} from "./services/toastService";
 
 @NgModule({
   declarations: [
@@ -31,8 +32,9 @@ import { AnimatedSlideComponent } from './pages/home/components/animated-slide/a
   ],
   exports: [MatButtonModule, MatCheckboxModule],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}, ToastService
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
