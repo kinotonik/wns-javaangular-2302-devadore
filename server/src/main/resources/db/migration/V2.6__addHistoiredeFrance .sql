c60ea54162051761eb8c49472ac06a2f054bec65
Mbirepinte <mickael.birepinte@example.com>
2023-09-15 09:20:22 +0200

    add Hf & Rugby quiz

--- server/src/main/resources/db/migration/V2.6__addHistoiredeFrance .sql
+++ server/src/main/resources/db/migration/V2.6__addHistoiredeFrance .sql
@@ -1,1 +1,1 @@
-INSERT INTO quizzforall.quiz (title, description, category_id, created_by, created_at, updated_at) VALUES ('Histoire de France','Histoire de France',3,3,'2023-02-14','2023-02-14');
+INSERT INTO quizzforall.quiz (id,title, description, category_id, created_by, created_at, updated_at) VALUES (5,'Histoire de France','Histoire de France',3,3,'2023-02-14','2023-02-14');
