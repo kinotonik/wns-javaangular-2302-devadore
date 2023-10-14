2009708dce9f17de7dff03c219c01e5dba39f790
Mbirepinte <mickael.birepinte@example.com>
2023-10-14 16:13:50 +0200

    add cors url

--- server/src/main/java/com/wcs/server/security/SecurityConfig.java
+++ server/src/main/java/com/wcs/server/security/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200","https://dev.quizz4all.lille-1.wilders.dev" ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
