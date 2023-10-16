b0d84427a70a606461af5ca8373ab0d060f04b86
Mbirepinte <mickael.birepinte@example.com>
2023-10-16 14:35:16 +0200

    allow all origin

--- server/src/main/java/com/wcs/server/security/SecurityConfig.java
+++ server/src/main/java/com/wcs/server/security/SecurityConfig.java
@@ -91,7 +91,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200","dev.quizz4all.lille-1.wilders.dev" ));
+        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200","dev.quizz4all.lille-1.wilders.dev" ));
+                configuration.setAllowedOrigins(Arrays.asList("*"));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList(
             "Authorization",
