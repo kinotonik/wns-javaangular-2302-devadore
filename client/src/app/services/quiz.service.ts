a5bfda1f69c644d5b81c4e72f17ba978714cb7e8
Mbirepinte <mickael.birepinte@example.com>
2023-10-18 08:48:14 +0200

    test change route

--- client/src/app/services/quiz.service.ts
+++ client/src/app/services/quiz.service.ts
@@ -1,49 +1,62 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable, map} from 'rxjs';
-import {QuizModel} from "../models/quiz.model";
-import {CreateQuizModel} from "../models/create-quiz.model";
-import {HeaderUtilService} from "./header-util.service";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { QuizModel } from '../models/quiz.model';
+import { CreateQuizModel } from '../models/create-quiz.model';
+import { HeaderUtilService } from './header-util.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuizService {
-  private apiUrl = 'http://localhost:8080/api/quiz';
+  private apiUrl = 'https://dev.quizz4all.lille-1.wilders.dev/api/quiz';
 
-  constructor(private http: HttpClient, private headerUtil: HeaderUtilService) {
-  }
+  constructor(
+    private http: HttpClient,
+    private headerUtil: HeaderUtilService
+  ) {}
 
   getQuizzs(): Observable<QuizModel[]> {
-    const headers = new HttpHeaders().set('Authorization', 'Basic QWRtaW46YWRtaW4=');
-    return this.http.get<QuizModel[]>(this.apiUrl, {headers});
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      'Basic QWRtaW46YWRtaW4='
+    );
+    return this.http.get<QuizModel[]>(this.apiUrl, { headers });
   }
 
   getQuizById(id: number): Observable<QuizModel> {
-    return this.http.get<QuizModel>(`${this.apiUrl}/show/${id}`, {headers: this.headerUtil.getHeaders()});
+    return this.http.get<QuizModel>(`${this.apiUrl}/show/${id}`, {
+      headers: this.headerUtil.getHeaders(),
+    });
   }
 
   getRandomQuiz(): Observable<QuizModel> {
-    return this.http.get<QuizModel>(this.apiUrl + '/random').pipe(
-      map((res) => res
-      ));
+    return this.http
+      .get<QuizModel>(this.apiUrl + '/random')
+      .pipe(map((res) => res));
   }
 
   getAllQuizzesCreatedByUser(userId: number): Observable<QuizModel[]> {
-    return this.http.get<QuizModel[]>(`${this.apiUrl}/${userId}`, {headers: this.headerUtil.getHeaders()});
+    return this.http.get<QuizModel[]>(`${this.apiUrl}/${userId}`, {
+      headers: this.headerUtil.getHeaders(),
+    });
   }
 
   createQuiz(formData: FormData): Observable<CreateQuizModel[]> {
-    return this.http.post<CreateQuizModel[]>(`${this.apiUrl}`, formData, {headers: this.headerUtil.getHeaders()});
+    return this.http.post<CreateQuizModel[]>(`${this.apiUrl}`, formData, {
+      headers: this.headerUtil.getHeaders(),
+    });
   }
 
-
   updateQuiz(id: number, formData: FormData): Observable<QuizModel[]> {
-    return this.http.put<QuizModel[]>(`${this.apiUrl}/${id}`, formData, {headers: this.headerUtil.getHeaders()});
+    return this.http.put<QuizModel[]>(`${this.apiUrl}/${id}`, formData, {
+      headers: this.headerUtil.getHeaders(),
+    });
   }
 
   deleteQuiz(id: number): Observable<QuizModel[]> {
-    return this.http.delete<QuizModel[]>(`${this.apiUrl}/${id}`, {headers: this.headerUtil.getHeaders()});
+    return this.http.delete<QuizModel[]>(`${this.apiUrl}/${id}`, {
+      headers: this.headerUtil.getHeaders(),
+    });
   }
 }
-
