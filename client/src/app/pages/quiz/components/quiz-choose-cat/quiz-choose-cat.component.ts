954e72f4eb844dd73e85b77c5d99568c4132aea8
Mbirepinte <mickael.birepinte@example.com>
2023-11-16 21:44:54 +0100

    working on cat service

--- client/src/app/pages/quiz/components/quiz-choose-cat/quiz-choose-cat.component.ts
+++ client/src/app/pages/quiz/components/quiz-choose-cat/quiz-choose-cat.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { QuizModel } from 'src/app/models/quiz.model';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-quiz-choose-cat',
   templateUrl: './quiz-choose-cat.component.html',
-  styleUrls: ['./quiz-choose-cat.component.css']
+  styleUrls: ['./quiz-choose-cat.component.css'],
 })
 export class QuizChooseCatComponent implements OnInit {
+  availableCategories: QuizModel[] = [];
+  constructor(private categoryService: CategoryService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  /* getAvailableCategories(): void {
+    this.categoryService.getAllCategories().subscribe((res) => {
+      this.availableCategories = res;
+      console.log(this.availableCategories);
+    });
+  } */
 }
