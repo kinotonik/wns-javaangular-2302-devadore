fb6dbccd7660b36a09a575843d5068b2683d5f9e
Mbirepinte <mickael.birepinte@example.com>
2023-12-12 22:13:44 +0100

    music button ok

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,18 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {AuthInterceptor} from "./auth/auth.interceptor";
-import {HomeComponent} from './pages/home/home.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from "@angular/material/button";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {MatTableModule} from "@angular/material/table";
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {ToastService} from "./services/toastService";
-import {ErrorComponent} from './pages/error-rediction/error/error.component';
-import {UnauthorizedComponent} from './pages/error-rediction/unauthorized/unauthorized.component';
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthInterceptor } from './auth/auth.interceptor';
+import { HomeComponent } from './pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ToastService } from './services/toastService';
+import { ErrorComponent } from './pages/error-rediction/error/error.component';
+import { UnauthorizedComponent } from './pages/error-rediction/unauthorized/unauthorized.component';
 
 @NgModule({
   declarations: [
@@ -34,9 +33,9 @@ import {UnauthorizedComponent} from './pages/error-rediction/unauthorized/unauth
   ],
   exports: [MatButtonModule, MatCheckboxModule],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}, ToastService
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    ToastService,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
