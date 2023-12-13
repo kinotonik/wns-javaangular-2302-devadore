7bb4ce40629b381a32a62d3487574da52cb5071d
Mbirepinte <mickael.birepinte@example.com>
2023-12-13 14:26:06 +0100

    add cat et correct edit quiz

---
+++ server/src/main/resources/db/migration/V5__addSomeCat.sql
@@ -0,0 +1,4 @@
+INSERT INTO quizzforall.category (name, description, created_at, updated_at) VALUES ('Musique', 'Es-tu un vrai mélomane?', '2023-01-02', '2023-01-02');  
+INSERT INTO quizzforall.category (name, description, created_at, updated_at) VALUES ('Séries TV', 'Questions sur les séries TV d''hier et d''aujourd''ui', '2023-01-02', '2023-01-02');  
+INSERT INTO quizzforall.category (name, description, created_at, updated_at) VALUES ('Littérature', 'Incollable sur la littérature?', '2023-01-02', '2023-01-02');  
+INSERT INTO quizzforall.category (name, description, created_at, updated_at) VALUES ('Cinéma', 'Es-tu un spécialiste du septième art?', '2023-01-02', '2023-01-02');  
