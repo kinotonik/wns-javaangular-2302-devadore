d2edc77524d18de1a68f021bed3a232abbb01eea
kinotonik <zedmed@kinotonik.fr>
2023-12-13 12:55:26 +0100

    feat: modif app module

--- client/src/app/pages/information/contact/contact.component.ts
+++ client/src/app/pages/information/contact/contact.component.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import {MatCardModule} from "@angular/material/card";
-import {SharedModule} from "../../../shared/shared.module";
 
 @Component({
   selector: 'app-contact',
